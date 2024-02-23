94cf98a5182434dfa8ed339daa28988e0ce3cb16
CongGenZ <nvcong25042003@gmail.comm>
2024-02-23 20:09:47 +0700

    git 3

--- src/com/tn/Program.java
+++ src/com/tn/Program.java
@@ -2,6 +2,8 @@ package com.tn;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git 1!");
+        System.out.println("Hello Git 2!");
+        System.out.println("Hello Git 3!");
     }
 }
